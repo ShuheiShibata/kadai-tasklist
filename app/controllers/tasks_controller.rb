1d43adda8576da32b7e913da0d785122fd76efac
shuuhei.shibata <46741432+ShuheiShibata@users.noreply.github.com>
2019-02-03 08:14:53 +0000

    add colum

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,13 +45,13 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
   
-  def task_set
+  def set_task
     @task = Task.find(params[:id])
   end
   
   #Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
 
