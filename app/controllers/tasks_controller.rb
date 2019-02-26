d16c9c0597fcd3c2daf0650ed09ff8db09a978d0
shuuhei.shibata <46741432+ShuheiShibata@users.noreply.github.com>
2019-02-26 13:39:39 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -13,14 +15,14 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Taskが正常に作成されました'
+      flash[:success] = 'タスクが正常に作成されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Taskが作成されませんでした'
-      render :new
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'タスクが作成されませんでした'
+      render 'toppages/index'
     end
   end
   
@@ -28,23 +30,22 @@ class TasksController < ApplicationController
   end
   
   def update
-
     if @task.update(task_params)
-      flash[:success] = 'Taskは正常に更新されました'
+      flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Taskは更新されませんでした'
+      flash.now[:danger] = 'タスクは更新されませんでした'
       render :edit
     end
   end
   
   def destroy
     @task.destroy
-    
-    flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_back(fallback_location: root_path)
   end
   
+  #private
   def set_task
     @task = Task.find(params[:id])
   end
@@ -52,6 +53,13 @@ class TasksController < ApplicationController
   #Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @tasks = current_user.tasks.find_by(id: params[:id])
+    unless @tasks
+      redirect_to root_url
+    end
   end
 end
 
